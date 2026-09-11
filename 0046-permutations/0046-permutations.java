eee4a080e3282194922e8a7b0913e6871a36a780
Guddheti Sathwik <139473024+Sathwik096@users.noreply.github.com>
2026-09-11 19:11:24 +0530

    Time: 1 ms (87.98%), Space: 45.6 MB (45.61%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,24 +1,26 @@
 class Solution {
-    static void add(int[] nums,List<List<Integer>> ans,List<Integer> cur,HashSet<Integer> set){
-        if(cur.size() == nums.length){
-            ans.add(new ArrayList<>(cur));
+    private static void swap(int i,int j,int[] nums){
+        int temp = nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    static void add(int i,int[] nums,List<List<Integer>> ans){
+        if(i == nums.length){
+            List<Integer> arr = new ArrayList<>();
+            for(int ind : nums)
+                arr.add(ind);
+            ans.add(new ArrayList<>(arr));
             return;
         }
-        for(int ind = 0 ; ind < nums.length; ind++){
-            if(!set.contains(ind)){
-                set.add(ind);
-                cur.add(nums[ind]);
-                add(nums,ans,cur,set);
-                cur.remove(cur.size()-1);
-                set.remove(ind);
-            }
+        for(int ind = i ; ind < nums.length; ind++){
+            swap(ind,i,nums);
+            add(i+1,nums,ans);
+            swap(ind,i,nums);
         }
     }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> cur = new ArrayList<>();
-        HashSet<Integer> set = new HashSet<>();
-        add(nums,ans,cur,set);
+        add(0,nums,ans);
         return ans;
     }
 }
